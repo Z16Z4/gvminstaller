e07aa2b626ebbd922e794e6a3d107f79538f9106
Z16Z4 <78325649+Z16Z4@users.noreply.github.com>
2022-02-24 18:40:58 +0000

    Update part5-run-as-root.py

--- part5-run-as-root.py
+++ part5-run-as-root.py
@@ -2,6 +2,11 @@
 #run as root part 4
 import os
 os.system("bash ./part4-set-export.sh")
+os.system("cp disable_thp.service /etc/systemd/system/disable_thp.service")
+os.system("cp gsad.service /lib/systemd/system/gsad.service")
+os.system("cp ospd-openvas.service /lib/systemd/system/ospd-openvas.service")
+os.system("cp gvmd.service /lib/systemd/system/gvmd.service")
+
 #MAKE GVM LIBS
 os.chdir("/tmp/gvm-source/gvm-libs")
 os.system("mkdir build")
@@ -55,6 +60,12 @@ os.system("make install")
 os.chdir("/tmp/gvm-source/ospd")
 os.system("pip3 install . --prefix=/opt/gvm")
 
+#MAKE OSPD-OPENVAS
+
+os.chdir("/tmp/gvm-source/ospd-openvas")
+os.system("pip3 install . --prefix=/opt/gvm")
+# ps aux | grep -E "ospd-openvas|gsad|gvmd" | grep -v grep
+
 
 #configuration
 os.system("cp /tmp/gvm-source/openvas/config/redis-openvas.conf /etc/redis")
@@ -71,4 +82,20 @@ os.system("systemctl enable redis-server@openvas")
 os.system("echo 'net.core.somaxconn = 1024' >> /etc/sysctl.conf")
 os.system("echo 'vm.overcommit_memory = 1' >> /etc/sysctl.conf")
 os.system("sysctl -p")
+os.system("systemctl daemon-reload")
+os.system("systemctl enable --now disable_thp")
+os.system("systemctl start redis-server@openvas")
+os.system("systemctl enable redis-server@openvas")
 
+#make sudoers
+os.system("echo 'gvm ALL = NOPASSWD: /opt/gvm/sbin/openvas' > /etc/sudoers.d/gvm")
+os.system("echo 'gvm ALL = NOPASSWD: /opt/gvm/sbin/gsad' >> /etc/sudoers.d/gvm")
+
+
+#make log 
+os.system("touch /opt/gvm/var/log/gvm/gsad.log")
+
+#start services
+os.system("systemctl enable gvmd.service; systemctl enable ospd-openvas.service; systemctl enable gsad.service; systemctl start gsad.service; systemctl start gvmd.service; systemctl start gsad.service")
+
+# ps aux | grep -E "ospd-openvas|gsad|gvmd" | grep -v grep
