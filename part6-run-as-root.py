9dcf461715a9b28470f148625b20b5e3202aa1a2
Z16Z4 <78325649+Z16Z4@users.noreply.github.com>
2022-02-24 18:43:51 +0000

    Update part6-run-as-root.py

--- part6-run-as-root.py
+++ part6-run-as-root.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
 
 
+import os 
+
+os.system("gvm-manage-certs -a")
+os.system("greenbone-nvt-sync")
+os.system("greenbone-feed-sync --type SCAP")
+os.system("gvmd --create-user=admin --password=letmein")
+os.system("gvmd --get-users --verbose")
+os.system("gvmd --modify-setting 78eceaec-3385-11ea-b237-28d24461215b --value 27790287-41de-49d3-be6e-daaec30348eb")
+
+
+
+#Feed sync
+
+os.system("greenbone-feed-sync --type GVMD_DATA")
+os.system("greenbone-feed-sync --type SCAP")
+os.system("greenbone-feed-sync --type CERT")
+
